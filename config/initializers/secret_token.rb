600a4556267406ee777f6b49cdd065aaeee0e370
Dan Jensen <daniel@pragmata-inc.com>
2012-10-30 12:52:01 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyReports::Application.config.secret_token = '2fd5f280fe03b7a01370ae5a0ca03e7863cc3487258c2ce37b4b8b6f261ea2bcd82f55ab12edaae54d037896ca4087800c2e78b3965850beca805f4e01b74a7e'
